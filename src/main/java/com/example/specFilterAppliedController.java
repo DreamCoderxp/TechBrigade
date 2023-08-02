04c183edfe29862042eb20aaada9b010498051dc
Msaini13314 <sin13314@sheridancollege.ca>
2023-08-02 14:24:11 -0400

    this is another commit after adding image in specification filter

--- src/main/java/com/example/specFilterAppliedController.java
+++ src/main/java/com/example/specFilterAppliedController.java
@@ -9,6 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class specFilterAppliedController {
@@ -23,17 +25,20 @@ public class specFilterAppliedController {
     private Label name111,cost111,ram111,storage111,processor111;
 
     @FXML
+    private ImageView img1, img11, img111;
+
+    @FXML
     private Button back;
 
 
     protected void showLaptops(ArrayList<Laptop> laptops) {
         if (laptops.size() >= 1) {
-            name1.setText(laptops.get(0).getName());
-            cost1.setText(Double.toString(laptops.get(0).getCost()));
-            ram1.setText(Integer.toString(laptops.get(0).getRam()));
-            storage1.setText(Integer.toString(laptops.get(0).getStorage()));
-            processor1.setText(laptops.get(0).getProcessor());
-            
+            name1.setText("Name: " + laptops.get(0).getName());
+            cost1.setText("Cost: " + Double.toString(laptops.get(0).getCost()));
+            ram1.setText("RAM: " + Integer.toString(laptops.get(0).getRam()));
+            storage1.setText("Storage: " + Integer.toString(laptops.get(0).getStorage()));
+            processor1.setText("Processor: " + laptops.get(0).getProcessor());
+            img1.setImage(new Image(laptops.get(0).getUrl()));
         } else {
             name1.setText("");
             cost1.setText("");
@@ -41,20 +46,23 @@ public class specFilterAppliedController {
             storage1.setText("");
             processor1.setText("");
         }
-    
+        
         if (laptops.size() >= 2) {
-            name11.setText(laptops.get(1).getName());
+            name11.setText("Name: " + laptops.get(1).getName());
             if (cost11 != null) {
-                cost11.setText(Double.toString(laptops.get(1).getCost()));
+                cost11.setText("Cost: " + Double.toString(laptops.get(1).getCost()));
             }
             if (ram11 != null) {
-                ram11.setText(Integer.toString(laptops.get(1).getRam()));
+                ram11.setText("RAM: " + Integer.toString(laptops.get(1).getRam()));
             }
             if (storage11 != null) {
-                storage11.setText(Integer.toString(laptops.get(1).getStorage()));
+                storage11.setText("Storage: " + Integer.toString(laptops.get(1).getStorage()));
             }
             if (processor11 != null) {
-                processor11.setText(laptops.get(1).getProcessor());
+                processor11.setText("Processor: " + laptops.get(1).getProcessor());
+            }
+            if (img11 != null) {
+                img11.setImage(new Image(laptops.get(1).getUrl()));
             }
         } else {
             name11.setText("");
@@ -71,20 +79,23 @@ public class specFilterAppliedController {
                 processor11.setText("");
             }
         }
-    
+        
         if (laptops.size() >= 3) {
-            name111.setText(laptops.get(2).getName());
+            name111.setText("Name: " + laptops.get(2).getName());
             if (cost111 != null) {
-                cost111.setText(Double.toString(laptops.get(2).getCost()));
+                cost111.setText("Cost: " + Double.toString(laptops.get(2).getCost()));
             }
             if (ram111 != null) {
-                ram111.setText(Integer.toString(laptops.get(2).getRam()));
+                ram111.setText("RAM: " + Integer.toString(laptops.get(2).getRam()));
             }
             if (storage111 != null) {
-                storage111.setText(Integer.toString(laptops.get(2).getStorage()));
+                storage111.setText("Storage: " + Integer.toString(laptops.get(2).getStorage()));
             }
             if (processor111 != null) {
-                processor111.setText(laptops.get(2).getProcessor());
+                processor111.setText("Processor: " + laptops.get(2).getProcessor());
+            }
+            if (img111 != null) {
+                img111.setImage(new Image(laptops.get(2).getUrl()));
             }
         } else {
             name111.setText("");
