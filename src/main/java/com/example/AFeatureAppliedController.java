a8d91c51e9648ae2224c571a8b34f7633c9edfa8
Msaini13314 <sin13314@sheridancollege.ca>
2023-08-02 16:37:19 -0400

    Jaskaarn additinoal feature shit

--- src/main/java/com/example/AFeatureAppliedController.java
+++ src/main/java/com/example/AFeatureAppliedController.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class AFeatureAppliedController {
@@ -22,29 +24,34 @@ public class AFeatureAppliedController {
     private Label name111,cost111,ram111, storage111, processor111,message111;
 
     @FXML
+    private ImageView img1,img11,img111;
+
+    @FXML
     private Label messageTop;
 
     @FXML
     protected void showLaptops(ArrayList<Laptop> laptops, String storedMessageTop, String storedMessageInner) {
 
-        name1.setText(laptops.get(0).getName());
-        cost1.setText(Double.toString(laptops.get(0).getCost()));
-        ram1.setText(Integer.toString(laptops.get(0).getRam()));
-        storage1.setText(Integer.toString(laptops.get(0).getStorage()));
-        processor1.setText(laptops.get(0).getProcessor());
-      
+        name1.setText("Name: " + laptops.get(0).getName());
+        cost1.setText("Cost: " + Double.toString(laptops.get(0).getCost()));
+        ram1.setText("RAM: " + Integer.toString(laptops.get(0).getRam()));
+        storage1.setText("Storage: " + Integer.toString(laptops.get(0).getStorage()));
+        processor1.setText("Processor: " + laptops.get(0).getProcessor());
+        img1.setImage(new Image(laptops.get(0).getUrl()));
 
-        name11.setText(laptops.get(1).getName());
-        cost11.setText(Double.toString(laptops.get(1).getCost()));
-        ram11.setText(Integer.toString(laptops.get(1).getRam()));
-        storage11.setText(Integer.toString(laptops.get(1).getStorage()));
-        processor11.setText(laptops.get(1).getProcessor());
-        
-        name111.setText(laptops.get(2).getName());
-        cost111.setText(Double.toString(laptops.get(2).getCost()));
-        ram111.setText(Integer.toString(laptops.get(2).getRam()));
-        storage111.setText(Integer.toString(laptops.get(2).getStorage()));
-        processor111.setText(laptops.get(2).getProcessor());
+        name11.setText("Name: " + laptops.get(1).getName());
+        cost11.setText("Cost: " + Double.toString(laptops.get(1).getCost()));
+        ram11.setText("RAM: " + Integer.toString(laptops.get(1).getRam()));
+        storage11.setText("Storage: " + Integer.toString(laptops.get(1).getStorage()));
+        processor11.setText("Processor: " + laptops.get(1).getProcessor());
+        img11.setImage(new Image(laptops.get(1).getUrl()));
+
+        name111.setText("Name: " + laptops.get(2).getName());
+        cost111.setText("Cost: " + Double.toString(laptops.get(2).getCost()));
+        ram111.setText("RAM: " + Integer.toString(laptops.get(2).getRam()));
+        storage111.setText("Storage: " + Integer.toString(laptops.get(2).getStorage()));
+        processor111.setText("Processor: " + laptops.get(2).getProcessor());
+        img111.setImage(new Image(laptops.get(2).getUrl()));
 
         // Set the message
         messageTop.setText(storedMessageTop);
